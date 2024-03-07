9b94533ef72d4355ceb9cc679dd88788d751cc55
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-07 21:27:19 +0000

    Adicionado programa que imprime expressões da consola com mais de 3 caráteres mas termina imediatamente caso tenham mais de 6

---
+++ RightSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string var in args)
+            {
+                if (var.Length > 3)
+                {
+                    if (var.Length > 6)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(var);
+                    }
+                }
+            }
+        }
+    }
+}

910c3f71a5e9cf3abeddcf97f581e48ce7ba5917
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-07 21:30:10 +0000

    adicionados comentários a explicar o que cada parte faz

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -8,14 +8,18 @@ namespace RightSize
         {
             foreach (string var in args)
             {
+                // determina se palavra > 3 caráteres
                 if (var.Length > 3)
                 {
+                    // determina se palavra > 6 caráteres
                     if (var.Length > 6)
                     {
+                        // termina o programa
                         break;
                     }
                     else
                     {
+                        // imprime as palavras com mais de 3 caráteres
                         Console.WriteLine(var);
                     }
                 }

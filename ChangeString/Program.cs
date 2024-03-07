41913a42cbcf0e2121aaa096eaadf6a597d39a5a
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-07 22:02:11 +0000

    Programa que muda todos os caracteres iguais ao fornecido pelo utilizador numa string fornecida pelo mesmo.

---
+++ ChangeString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pede uma string ao utilizador
+            Console.Write("String: ");
+            string input1 = Console.ReadLine();
+
+            // Pede um caráter ao utilizador
+            Console.Write("Char: ");
+            string input2 = Console.ReadLine();
+            // Passa de string a char
+            char cara = char.Parse(input2);
+
+            // Define a string final
+            string stringFinal = "";
+
+            // Procura todos os carateres
+            foreach (char a in input1)
+            {
+                // Se for igual ao carater do utilizador 
+                if (a == cara)
+                {
+                    // Troca por X
+                    stringFinal += "X";
+                }
+                // Se não
+                else
+                {
+                    // Coloca igual
+                    stringFinal += a;
+                }
+            }
+
+            // Imprime string final
+            Console.WriteLine(stringFinal);
+
+        }
+    }
+}

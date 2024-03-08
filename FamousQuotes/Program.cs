3b143a9b409bde54dd9567a9f33b1bc8ad41ae6e
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-08 00:32:03 +0000

    You talking to me? My precious. Show me the money! Dodge this. Hasta la vista, baby.

---
+++ FamousQuotes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FamousQuotes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Taxi driver
+            Console.WriteLine("You talking to me?");
+            MyPrecious();
+            
+            // Matrix
+            Console.WriteLine("Dodge this");
+            Hasta();
+        }
+
+        private static void Hasta()
+        {
+            TheMoney();
+
+            // Terminator
+            Console.WriteLine("Hasta la vista, baby.");
+            MyPrecious();
+        }
+
+        private static void TheMoney()
+        {
+            // Jerry Maguire
+            Console.WriteLine("Show me the money!");
+        }
+
+        private static void MyPrecious()
+        {
+            TheMoney();
+
+            // Lord of the rings
+            Console.WriteLine("My precious.");
+        }
+    }
+}
